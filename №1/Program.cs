81892e244ad1d74ea6f9a54ce300450df63e4328
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-21 14:19:40 +0200

    Не решена 1 задача

--- №1/Program.cs
+++ №1/Program.cs
@@ -4,11 +4,13 @@ Console.WriteLine("Введите пятизначное число ");
 int number = Convert.ToInt32(Console.ReadLine());
 string strnum = Convert.ToString(number);
 int five = number%10000;
+
 while ( number == five || number == five)
 {
     Console.WriteLine("Неверное число, введите пятизначное ");
-    Convert.ToInt32(Console.ReadLine());
+    int number2 = Convert.ToInt32(Console.ReadLine());    
 }
+
 if (strnum[0] == strnum[4] && strnum[1] == strnum[3])
 {
     Console.WriteLine("Это число являеться палиндромом");
